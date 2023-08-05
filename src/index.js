ef0558949718baa51c330fd595c10219167ab560
tyler-netek <121643071+tyler-netek@users.noreply.github.com>
2023-08-05 15:46:05 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 import About from './About';
 import reportWebVitals from './reportWebVitals';
 import Resume from './Resume';
