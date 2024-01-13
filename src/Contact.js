d516216c04461041ded881bb3b2005345616e142
tyler-netek <121643071+tyler-netek@users.noreply.github.com>
2024-01-13 08:14:41 -0500

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -85,11 +85,11 @@ const Contact = () => {
       {submitted && <div className="submitted-message">Submitted!</div>}
     </div>
     <div class="image-container">
-      <hr>
-      <img src="./images/main.png" alt="Home Page (Predictr)">
-      <img src="./images/searchbar.png" alt="Search Results (Predictr)">
-      <img src="./images/chart.png" alt="Chart (Predictr)">
-      <hr>
+        <hr />
+        <img src="./images/main.png" alt="Home Page (Predictr)" />
+        <img src="./images/searchbar.png" alt="Search Results (Predictr)" />
+        <img src="./images/chart.png" alt="Chart (Predictr)" />
+        <hr />
     </div>
   );
 };
