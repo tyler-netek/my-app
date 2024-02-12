45966475ba3dd89aea786317b719a4628dceca10
tyler-netek <121643071+tyler-netek@users.noreply.github.com>
2024-02-12 17:34:17 -0500

    Create Photos.js

---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import chart from './images/chart.png';
+import main from './images/main.png';
+import search from './images/searchbar.png'; 
+import React from 'react';
+import './Photos.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img src={main} alt="Image 1" />
+      </div>
+      <div className="image-container">
+        <img src={search} alt="Image 2" />
+      </div>
+      <div className="image-container">
+        <img src={chart} alt="Image 3" />
+      </div>
+    </div>
+  );
+}
+
+export default Photos;
