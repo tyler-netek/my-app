e173e1cf459021f08218bfaf821cb20ff72fbae1
tyler-netek <121643071+tyler-netek@users.noreply.github.com>
2024-02-12 19:05:28 -0500

    Fixes

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import Resume from './Resume';
 import Contact from './Contact';
 import Footer from './Footer';
 import Photos from './Photos';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
       <Navbar />
+      <App />
       <About />
       <Resume />
       <Contact />
